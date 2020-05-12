b3ae445ba75476ebfa2a8ee9200b05f2bb8d2ff0
Steven Hobson-Campbell <xapphire13@live.com>
2020-05-11 23:43:32 -0700

    update gradle for jetpack

--- app/src/main/java/com/xapphire13/calendarwidget/CalendarAppWidgetProvider.kt
+++ app/src/main/java/com/xapphire13/calendarwidget/CalendarAppWidgetProvider.kt
@@ -1,6 +1,31 @@
 package com.xapphire13.calendarwidget
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import java.util.stream.IntStream
 
 class CalendarAppWidgetProvider : AppWidgetProvider() {
+  override fun onUpdate(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetIds: IntArray
+  ) {
+    super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+    appWidgetIds.forEach {
+      val rv = RemoteViews(context.packageName, R.layout.widget)
+      rv.removeAllViews(R.id.item_container)
+
+      IntStream.range(0, 5).forEach { i ->
+        val item = RemoteViews(context.packageName, R.layout.calendar_item)
+        item.setTextViewText(R.id.calendar_item_text, "Test" + (i + 1))
+
+        rv.addView(R.id.item_container, item);
+      }
+
+      appWidgetManager.updateAppWidget(it, rv)
+    }
+  }
 }
